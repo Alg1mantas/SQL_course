a3a09509680c81e16af3b5c7d9d0dc41ae8931cf
AlgimantasKirdeikis <kirdeikisalgimantas@gmail.com>
2022-09-01 18:54:21 +0300

    create CRUD methods

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from sqlite3 import DatabaseError
 
 class SqlDatabase:
     def __init__(self, db_name: str) -> None:
-        self._conn = sqlite3.connect(db_name)
+        self._conn = sqlite3.connect(db_name+".db")
         self._cursor = self._conn.cursor()
 
     def create_table(self, table_name: str, columns: str) -> None:
@@ -23,23 +23,31 @@ class SqlDatabase:
         except Exception as e:
             print(f"Unable to create table!. Error msg: {e}")
 
-    def create(self, table_name: str, entry_values: str) -> None:
+    def write(self, table_name: str, entry_values: str) -> None:
         with self._conn:
             self._cursor.execute(f"INSERT INTO {table_name} VALUES ({entry_values})")
 
-    def read(self):
-        pass
+    def read(self, columns: str, table_name: str, where_clause: str) -> list:
+        with self._conn:
+            self._cursor.execute(f"SELECT ({columns}) From {table_name} WHERE {where_clause}")
+            return self._cursor.fetchall()
 
-    def update(self):
-        pass
+    def update(self, table_name: str, update_clause: str, where_clause: str) -> None:
+        with self._conn:
+            self._cursor.execute(f"UPDATE ({table_name}) SET {update_clause} WHERE {where_clause}'")
 
-    def delete(self):
-        pass
+    def delete(self, table_name: str, where_clause: str) -> None:
+        with self._conn:
+            self._cursor.execute(f"DELETE from {table_name} WHERE {where_clause}")
 
 def main():
-    dtbs = SqlDatabase("Food")
+    db = SqlDatabase("Food")
+    db.create_table("People", "name text, surname text, age integer")
+    # db.write(table_name="People", entry_values="'Jonas', 'Kazlauskas', 69")
+    print(db.read(columns="surname",table_name="People", where_clause="age=69"))
     
 
 
 if __name__ == "__main__":
     main()
+    print("Done")
